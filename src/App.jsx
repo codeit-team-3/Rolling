7429a8736f4a56fb2812bc938ce4d25cd616f9e2
유성진 <ysj7645@gmail.com>
2024-04-30 21:40:32 +0900

    Revert "폰트 설정 및 컴포넌트 밑작업"
    This reverts commit ca120c52f073b1967bff53be2f3d4978aebd9a1d.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,5 @@
-import Badge from "./components/Badge"
-import Toast from "./components/Toast"
-import ToggleButton from "./components/ToggleButton"
-
 function App() {
-  return (
-    <div>
-      <Badge />
-      <Toast />
-      <ToggleButton />
-    </div>
-  )
+  return <></>
 }
 
 export default App
--- src/components/Toast.jsx
+++
@@ -1,5 +0,0 @@
-const Toast = () => {
-  return <div>URL이 복사 되었습니다.</div>
-}
-
-export default Toast
