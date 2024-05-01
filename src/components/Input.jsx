3f2d19ef908b0e81f40959021167fb6128a26cda
Park-Jingyeong <pjg0305pjg@gmail.com>
2024-05-01 14:03:05 +0900

    TR-6-feat: Error Message 추가

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,21 +3,34 @@ import { useState } from "react"
 
 const Input = () => {
   const [text, setText] = useState("")
+  const [isTouched, setIsTouched] = useState(false)
+  const [isFocused, setIsFocused] = useState(false)
 
   const onChange = (e) => {
     setText(e.target.value)
   }
 
+  const onBlur = () => {
+    setIsTouched(true)
+    setIsFocused(false)
+  }
+
+  const onFocus = () => {
+    setIsFocused(true)
+  }
+
+  const hasError = isTouched && !isFocused && text.trim() === ""
   return (
     <div>
-      <label htmlFor="userInput"></label>
       <input
-        className={styles["input"]}
-        id="userInput"
+        className={hasError ? styles.errorInput : styles.input}
         value={text}
         onChange={onChange}
+        onBlur={onBlur}
+        onFocus={onFocus}
         placeholder="Placeholder"
       />
+      {hasError && <p className={styles.errorMessage}>Error Message</p>}
     </div>
   )
 }
