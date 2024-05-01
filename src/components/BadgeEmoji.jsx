bddab70911c084d02707214aa827c68ac6ae32bf
eprnf <eprnf21@gmail.com>
2024-05-01 12:47:33 +0900

    TR-27-refactor: api와 동일하게 prop명 변경(number->count)

--- src/components/BadgeEmoji.jsx
+++ src/components/BadgeEmoji.jsx
@@ -2,17 +2,17 @@ import React from "react"
 import PropTypes from "prop-types"
 import styles from "./BadgeEmoji.module.css"
 
-const BadgeEmoji = ({ emoji, number }) => {
+const BadgeEmoji = ({ emoji, count }) => {
   return (
     <span className={styles.badgeEmoji}>
-      {emoji} {number}
+      {emoji} {count}
     </span>
   )
 }
 
 BadgeEmoji.propTypes = {
   emoji: PropTypes.string,
-  number: PropTypes.number,
+  count: PropTypes.number,
 }
 
 export default BadgeEmoji
