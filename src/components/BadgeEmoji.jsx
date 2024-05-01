4fa0eafd3474befb293cb5433e476d7aaef5ed61
eprnf <eprnf21@gmail.com>
2024-05-01 12:47:33 +0900

    TR-27-refactor: prop명 수정(number -> count)

--- src/components/BadgeEmoji.jsx
+++ src/components/BadgeEmoji.jsx
@@ -2,10 +2,10 @@ import React from "react"
 import PropTypes from "prop-types"
 import styles from "./BadgeEmoji.module.css"
 
-const BadgeEmoji = ({ emoji, number }) => {
+const BadgeEmoji = ({ emoji, count }) => {
   return (
     <span className={styles.badgeEmoji}>
-      {emoji} {number}
+      {emoji} {count}
     </span>
   )
 }
