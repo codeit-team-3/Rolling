5aacb8f992e3855825b497942a54ff2a2706eb1b
eprnf <eprnf21@gmail.com>
2024-05-02 10:28:36 +0900

    TR-13-feat: 서비스헤더 내부에서 사용되는 컴포넌트 구현

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
-import './fonts/fonts.css'
+import "./fonts/fonts.css"
 import "./colors/colors.module.css"
 import "./styles/icons.css"
 
