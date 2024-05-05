761f32861c720885e2205b681ca68e01b987df6e
강은우 <80903685+eunwoo66@users.noreply.github.com>
2024-05-05 15:21:59 +0900

    TR-15-refactor: util 수정

--- src/services/utils.js
+++ src/services/utils.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const TEAM_ID = "1-4"
+const TEAM_ID = "6-3"
 const BASE_URL = `https://rolling-api.vercel.app/${TEAM_ID}`
 
 export const axiosInstance = axios.create({
@@ -9,9 +9,9 @@ export const axiosInstance = axios.create({
 })
 
 export function formatDateString(dateString) {
-  const date = new Date(dateString);
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
-  return `${year}.${month}.${day}`;
+  const date = new Date(dateString)
+  const year = date.getFullYear()
+  const month = String(date.getMonth() + 1).padStart(2, "0")
+  const day = String(date.getDate()).padStart(2, "0")
+  return `${year}.${month}.${day}`
 }
