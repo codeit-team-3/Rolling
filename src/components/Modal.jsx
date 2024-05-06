1761ade562427fa6c6365e7f156a99719cf3eb5b
강은우 <80903685+eunwoo66@users.noreply.github.com>
2024-05-06 19:12:10 +0900

    TR-41-refactor: modal 컴포넌트 함수 정리

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,14 +5,6 @@ import ProfileImage from "./ProfileImage"
 import { formatDateString } from "../utils/formatDateString"
 import styles from "./Modal.module.css"
 
-function formatDateString(dateString) {
-  const date = new Date(dateString)
-  const year = date.getFullYear()
-  const month = String(date.getMonth() + 1).padStart(2, "0")
-  const day = String(date.getDate()).padStart(2, "0")
-  return `${year}.${month}.${day}`
-}
-
 const Modal = ({ messageData }) => {
   if (!messageData) {
     return <div className={`${styles.modal} ${styles.loading}`}>Loading...</div>
