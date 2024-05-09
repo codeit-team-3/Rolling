6f13b4686a4b4db1bf94460988ca086493de655c
MJY1993 <mjylove26@gmail.com>
2024-05-09 14:42:26 +0900

    TR-14-refactor: cardList 2차 수정

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,23 +1,19 @@
-import styles from './CardList.module.css'
+import styles from "./CardList.module.css"
 import ContributorsInfo from "./ContributorsInfo"
 import BadgeEmoji from "./BadgeEmoji"
 
-
 const CardList = ({
-  backgroundColor, 
+  backgroundColor,
   backgroundImageURL,
-  name, 
-  recentMessages, 
-  messageCount, 
-  topReactions
-  }) => {
-    
+  name,
+  recentMessages,
+  messageCount,
+  topReactions,
+}) => {
   return (
     <div className={styles.container}>
       <div className={`${styles.cardList} ${styles[backgroundColor]}`}>
-        <section className={styles.name}>
-          {`To. ${name}`}
-        </section>
+        <section className={styles.name}>{`To. ${name}`}</section>
         <section className={styles.contributors}>
           <ContributorsInfo
             recentMessages={recentMessages}
@@ -25,16 +21,14 @@ const CardList = ({
           />
         </section>
         <section className={styles.reactions}>
-          {topReactions.map((item) => { 
-            return <BadgeEmoji 
-                      key={item.id} 
-                      count={item.count} 
-                      emoji={item.emoji} 
-                    />
+          {topReactions.map((item) => {
+            return (
+              <BadgeEmoji key={item.id} count={item.count} emoji={item.emoji} />
+            )
           })}
         </section>
+      </div>
     </div>
-   </div>  
   )
 }
 
