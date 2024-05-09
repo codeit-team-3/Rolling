007fe4cad437abbccc063d6c4aa62f3623ac01ee
MJY1993 <mjylove26@gmail.com>
2024-05-09 15:46:22 +0900

    feat: testing swiper

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,10 +1,38 @@
 import styles from "./List.module.css"
-import { useState, useEffect, useCallback } from "react"
+import { useState, useEffect, useCallback, useRef } from "react"
 import CardList from "../components/CardList"
 import ButtonPrimary from "../components/ButtonPrimary"
 import useRequest from "../hooks/useRequest"
 import React from "react"
 import { useNavigate } from "react-router-dom"
+// core version + navigation, pagination modules:
+// import Swiper from 'swiper';
+import { Swiper, SwiperSlide } from "swiper/react"
+// import Swiper and modules styles
+import "swiper/css"
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules"
+
+// const List = () => {
+
+//   return (
+//     <div className={styles.container}>
+//       <Swiper
+//         module={[Navigation, EffectFade]}
+//         Navigation
+//         effect
+//         speed={800}
+//         slidesPerView={1}
+//         loop
+//         className={styles.myswiper}
+//       >
+//         <SwiperSlide className={styles.swiperslide}>
+
+//         </SwiperSlide>
+//       </Swiper>
+//     </div>
+//   )
+// }
 
 const RECIPIENTS_LIMIT = 4
 
@@ -34,9 +62,24 @@ const List = () => {
       <section className={styles.wrapper}>
         <div className={styles.header}>인기 롤링 페이퍼 🔥</div>
         <div className={styles.list}>
-          {data.map((item) => {
-            return <CardList key={item.id} {...item} />
-          })}
+          <Swiper
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
+            spaceBetween={50}
+            slidesPerView={3}
+            navigation
+            pagination={{ clickable: true }}
+            scrollbar={{ draggable: true }}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log("slide change")}
+          >
+            {data.map((item) => {
+              return (
+                <SwiperSlide key={item.id}>
+                  <CardList {...item} />
+                </SwiperSlide>
+              )
+            })}
+          </Swiper>
         </div>
       </section>
       <secion className={styles.wrapper}>
