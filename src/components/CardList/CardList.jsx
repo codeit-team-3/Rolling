93d9294899e3ad6d6dce12a37a677076e7538737
eprnf <eprnf21@gmail.com>
2024-05-09 20:34:51 +0900

    TR-50-refactor: 컴포넌트 코드 일부 수정 및 디자인 시안과 동일하게 수정

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,6 +1,17 @@
+import ContributorsInfo from "../ContributorsInfo/ContributorsInfo"
+import BadgeEmoji from "../BadgeEmoji/BadgeEmoji"
 import styles from "./CardList.module.css"
-import ContributorsInfo from "./ContributorsInfo"
-import BadgeEmoji from "./BadgeEmoji"
+import beigeImage from "../../assets/img-beige.png"
+import greenImage from "../../assets/img-green.png"
+import purpleImage from "../../assets/img-purple.png"
+import blueImage from "../../assets/img-blue.png"
+
+const BACKGROUND_COLOR_IMAGE = {
+  beige: beigeImage,
+  green: greenImage,
+  purple: purpleImage,
+  blue: blueImage,
+}
 
 const CardList = ({
   backgroundColor,
@@ -10,24 +21,48 @@ const CardList = ({
   messageCount,
   topReactions,
 }) => {
+  const BACKGROUND = {
+    beige: { backgroundColor: "#ffe2ad" },
+    green: { backgroundColor: "#d0f5c3" },
+    purple: { backgroundColor: "#ecd9ff" },
+    blue: { backgroundColor: "#b1e4ff" },
+    image: {
+      color: "var(--white)",
+      backgroundImage: `url(${backgroundImageURL})`,
+    },
+  }
+
   return (
-    <div className={styles.container}>
-      <div className={`${styles.cardList} ${styles[backgroundColor]}`}>
-        <section className={styles.name}>{`To. ${name}`}</section>
-        <section className={styles.contributors}>
-          <ContributorsInfo
-            recentMessages={recentMessages}
-            messageCount={messageCount}
-          />
-        </section>
-        <section className={styles.reactions}>
-          {topReactions.map((item) => {
-            return (
-              <BadgeEmoji key={item.id} count={item.count} emoji={item.emoji} />
-            )
-          })}
-        </section>
-      </div>
+    <div
+      className={styles.cardList}
+      style={
+        backgroundImageURL ? BACKGROUND.image : BACKGROUND[backgroundColor]
+      }
+    >
+      {backgroundImageURL ? (
+        <div className={styles.overlay}></div>
+      ) : (
+        <img
+          className={styles.colorImage}
+          src={BACKGROUND_COLOR_IMAGE[backgroundColor]}
+        ></img>
+      )}
+
+      <section className={styles.content}>
+        <div className={styles.name}>{`To. ${name}`}</div>
+        <ContributorsInfo
+          recentMessages={recentMessages}
+          messageCount={messageCount}
+        />
+      </section>
+
+      <section className={styles.reactions}>
+        {topReactions.map((item) => {
+          return (
+            <BadgeEmoji key={item.id} count={item.count} emoji={item.emoji} />
+          )
+        })}
+      </section>
     </div>
   )
 }
